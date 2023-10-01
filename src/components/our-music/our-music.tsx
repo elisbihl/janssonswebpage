8d84f1fa85a347bdd0cc57193b19f44209d61bca
Joel Kågemyr <kagemyrjoel@gmail.com>
2023-10-01 13:33:47 +0200

    Socials done

--- src/components/our-music/our-music.tsx
+++ src/components/our-music/our-music.tsx
@@ -15,7 +15,7 @@ export class OurMusic {
             class="spotify-player"
             src="https://open.spotify.com/embed/album/3z0zICSQQj9ZNNO7c7xXTD?utm_source=generator&theme=0"
             width="100%"
-            height="352"
+            height="460"
             frameBorder="0"
             allowfullScreen=""
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
--- src/components/social-media/social-media.tsx
+++ src/components/social-media/social-media.tsx
@@ -1,22 +1,46 @@
-import { Component, h } from '@stencil/core';
+import {getAssetPath, Component, h } from '@stencil/core';
 
 @Component({
   tag: 'social-media',
   styleUrl: 'social-media.css',
   shadow: true,
+  assetsDirs: ["../../assets/images"]
 })
+
 export class SocialMedia {
 
   render() {
     return (
-      <div class="container">
-        <div id="socials">
-          <ul>
-            <li class="social-link"><a>Spotify</a></li>
-            <li class="social-link"><a>Facebook</a></li>
-            <li class="social-link"><a>Instagram</a></li>
-            <li class="social-link"><a>Tiktok</a></li>
-          </ul>
+      <div class="content">
+         <h2> Socials </h2>
+        <div class="socials-row" id="socials">
+         <a href="https://instagram.com/janssonstemptationxp">
+             <img src={getAssetPath('../../assets/images/instagram.png')} alt=""/>
+         </a>
+         <a href="https://open.spotify.com/artist/6phazupSvTafjFXdr9wrtn">
+             <img src={getAssetPath('../../assets/images/spotify.png')} alt=""/>
+         </a>
+         <a href="https://www.facebook.com/thejanssonstemptation">
+             <img src={getAssetPath('../../assets/images/facebook.png')} alt=""/>
+         </a>
+         <a href="https://jtxp.bandcamp.com/">
+             <img src={getAssetPath('../../assets/images/bandcamp.png')} alt=""/>
+         </a>
+         <a href="https://www.tiktok.com/@janssonstemptation">
+             <img src={getAssetPath('../../assets/images/tik-tok.png')} alt=""/>
+         </a>
+         <a href="https://soundcloud.com/janssonstemptation">
+             <img src={getAssetPath('../../assets/images/soundcloud.png')} alt=""/>
+         </a>
+         <a href="https://music.apple.com/se/artist/the-janssons-temptation-experience">
+             <img src={getAssetPath('../../assets/images/social.png')} alt=""/>
+         </a>
+         <a href="https://www.youtube.com/channel/UCUCpBQizFVYJDH8B4CayjDg/featured?sub_confirmation=1">
+             <img src={getAssetPath('../../assets/images/youtube.png')} alt=""/>
+         </a>
+         <a href="mailto:janssons2021@gmail.com">
+             <img src={getAssetPath('../../assets/images/email.png')} alt=""/>
+         </a>
         </div>
       </div>
     );
